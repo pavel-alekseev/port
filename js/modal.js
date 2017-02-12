8027ef13af5f814c78cfaaa808f22ca17c950f00
Pavel Alekseev <pavelmorr@gmail.com>
2017-02-12 12:31:18 +0200

    1

---
+++ js/modal.js
@@ -0,0 +1,53 @@
+function showa() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#a").addClass("fade").modal("show");
+}
+
+$(".dialog-a").on("click",function(){
+    showa();
+});
+
+function showb() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#b").addClass("fade").modal("show");
+}
+
+$(".dialog-b").on("click",function(){
+    showb();
+});
+
+function showc() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#c").addClass("fade").modal("show");
+}
+
+$(".dialog-c").on("click",function(){
+    showc();
+});
+
+function showd() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#d").addClass("fade").modal("show");
+}
+
+$(".dialog-d").on("click",function(){
+    showd();
+});
+
+function showe() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#e").addClass("fade").modal("show");
+}
+
+$(".dialog-e").on("click",function(){
+    showe();
+});
+
+function showf() {
+    $(".in").removeClass("fade").modal("hide");
+    $("#f").addClass("fade").modal("show");
+}
+
+$(".dialog-f").on("click",function(){
+    showf();
+});
